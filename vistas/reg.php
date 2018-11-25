117bb746a6e01e8a81161fe395c56c5c42349345
Raul <raul_leandro_0504@hotmail.com>
2018-11-25 00:19:35 -0400

    se modificaron el registro y el login para insertar metodos en los formularios, con reg.php se conecto con la base de datos mysql con php directo en el servidor de produccion 000webhost, se probo la insercion de datos a la bd a traves de los formularios, funciono. sin validaciones. ignorar archivos firebase en el repo local

---
+++ vistas/reg.php
@@ -0,0 +1,27 @@
+<?php
+    //conexion con la base de datos
+    $servername = "localhost";
+    $database = "id7622858_criptoapp";
+    $username = "id7622858_raulleandro";
+    $password = "totti0504@";
+    // Create connection
+    $conexion = mysqli_connect($servername, $username, $password, $database) or die ("No se ha podido conectar al servidor de Base de datos");
+    $db = mysqli_select_db( $conexion, $database )or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );;
+
+
+    //guarda los datos del formulario
+    $nombre = $_POST['nombreu'];
+    $apellido = $_POST['apellidou'];
+    $email = $_POST['emailu'];
+    $password = $_POST['passwordu'];
+    $pass = md5($password);
+ 
+    $query = "INSERT INTO usuario VALUES ('$nombre','$apellido','$email','$pass')";
+    //envia a la base de datos
+    mysqli_query($conexion, $query) or die ("<h2>error al enviar datos a la bs</h2>"); 
+
+    //confirmacion
+    echo '<h2>se lograron enviar los datos</h2>';
+
+    mysqli_close($conexion);
+?>
